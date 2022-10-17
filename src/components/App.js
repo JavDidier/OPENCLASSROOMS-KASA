4041dfe93983b48a20c1cc4f9a33b7bfd9373607
JavDidier <javierre.didier@gmail.com>
2022-10-17 10:45:05 +0200

    Étape 3 : routeur et pages fonctionnelles

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-function App() {
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from '../pages/Home';
+import Product from '../pages/Product';
+import About from '../pages/About';
+import Error from '../pages/Error';
+
+
+const App = () => {
   return (
-    <div>
-      <h1>Kasa</h1>
-    </div>
-  );
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<Home />} />
+        <Route path="/about" element= {<About />} />
+        <Route path="/product" element= {<Product />} />
+        <Route path="*" element= {<Error />} />
+      </Routes>
+    </BrowserRouter>
+);
+};
 
 export default App;
